3f8119c0ce89b59c151dc032daecf181f2d33be1
jaswanth-hi <jaswanthkumarreddy111@gmail.com>
2024-11-21 14:39:54 +0530

    first commit

---
+++ fontend/vite-project/src/Home.jsx
@@ -0,0 +1,12 @@
+import Create from "../Create"
+
+function Home(){
+    return(
+        <div>
+            <h1>Todo List</h1>
+            <Create/>
+
+        </div>
+    )
+}
+export default Home
